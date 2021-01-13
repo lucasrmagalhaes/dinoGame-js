656de19a889617b63ee3c0199ab0f4a00ab81410
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-13 20:55:51 -0300

    interval configurado

--- script.js
+++ script.js
@@ -2,8 +2,33 @@ const dino = document.querySelector('.dino');
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
-        
+        jump();
     }
+}
+
+function jump() {
+    let position = 0;
+
+    let upInterval = setInterval(() => {
+        
+        if (position >= 150) {
+            clearInterval(upInterval);
+        
+        // Descendo.
+        let downInterval = setInterval(() => {
+            if (position <= 0) {
+                clearInterval(downInterval);
+            } else {
+                position -= 20;
+                dino.style.bottom = position + 'px';
+            }
+        }, 20);
+        } else {
+        // Subindo.
+        position += 20;
+        dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);

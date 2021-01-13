46ff4e29506a22fe6ee0cd899b83336fbc332f67
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-13 20:39:32 -0300

    tecla space configurada

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# dinoGame-js
-Recriando o famoso jogo do dinossauro sem internet.
+
+
+<p align="left">
+    <a href="keycode.info">keycode.info</a>
+</p>
---
+++ script.js
@@ -0,0 +1,9 @@
+const dino = document.querySelector('.dino');
+
+function handleKeyUp(event) {
+    if (event.keyCode === 32) {
+        
+    }
+}
+
+document.addEventListener('keyup', handleKeyUp);

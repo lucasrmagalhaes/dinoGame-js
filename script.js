69382d01768705e74647ef43576b09f856feb869
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-13 21:07:06 -0300

    cactus criado

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background');
 let isJumping = false;
 
 function handleKeyUp(event) {
@@ -37,4 +38,14 @@ function jump() {
     }, 20);
 }
 
+function createCactus() {
+    const cactus = document.createElement('div');
+    let cactusPosition = 1000;
+
+    cactus.classList.add('cactus');
+    cactus.style.left = 1000 + 'px';
+    background.appendChild(cactus);
+}
+
+createCactus();
 document.addEventListener('keyup', handleKeyUp);

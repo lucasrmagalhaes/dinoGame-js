630891a17314d02fbcd622dee1d8b073b7a189d1
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-01-13 21:52:20 -0300

    jogo finalizado

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -11,7 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0;
     
     isJumping = true;
 
@@ -39,11 +39,11 @@ function jump() {
 }
 
 function createCactus() {
+    
     const cactus = document.createElement('div');
+    
     let cactusPosition = 1000;
     let randomTime = Math.random() + 6000;
-
-
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
@@ -56,12 +56,15 @@ function createCactus() {
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            // Game Over.
+            clearInterval(leftInterval);
+            document.body.innerHTML = '<h1 class="game-over">GAME OVER</h1>';
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
         }
     }, 20);
-
     setTimeout(createCactus, randomTime);
 }
 
